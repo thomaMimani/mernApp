4cfd03ce74e65cd5c34a85c6936c627897bfd98c
Thoma Mimani <thoma.mimani@onlinenow.eu>
2023-01-14 20:56:09 +0100

    Check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Login } from './components/Login/Login';
-
+console.log('hello')
 function App() {
   return (
       <Routes>
         <Route path='/' element={<Login/>}/>
-        
       </Routes>
   );
 }
